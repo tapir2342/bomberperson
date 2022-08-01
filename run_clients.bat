4b6e5629784b203f2b426a27b8851674e3750c71
tapir2342 <github@tapir2342.com>
2022-08-01 20:10:43 +0200

    Cool

---
+++ run_clients.bat
@@ -0,0 +1,6 @@
+@echo off
+
+start C:\Users\kai\bin\Godot_v3.4.4-stable_win64.exe .
+#start C:\Users\kai\bin\Godot_v3.4.4-stable_win64.exe .
+#start C:\Users\kai\bin\Godot_v3.4.4-stable_win64.exe .
+#start C:\Users\kai\bin\Godot_v3.4.4-stable_win64.exe .
---
+++ run_server.bat
@@ -0,0 +1,3 @@
+@echo off
+
+C:\Users\kai\bin\Godot_v3.4.4-stable_win64.exe --server .
