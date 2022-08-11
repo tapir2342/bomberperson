32ae1582823ac1fd4b82fccd7eb566361586c203
tapir2342 <github@tapir2342.com>
2022-08-11 18:46:16 +0200

    Run window without server; position clients

--- run_all.bat
+++ run_all.bat
@@ -1,5 +1,5 @@
 @echo off
 
-start C:\Users\kai\bin\Godot_v3.5-stable_win64.exe --server .
-start C:\Users\kai\bin\Godot_v3.5-stable_win64.exe .
-start C:\Users\kai\bin\Godot_v3.5-stable_win64.exe .
+start C:\Users\kai\bin\Godot_v3.5-stable_win64.exe --no-window --server .
+start C:\Users\kai\bin\Godot_v3.5-stable_win64.exe --position 500,400 .
+start C:\Users\kai\bin\Godot_v3.5-stable_win64.exe --position 2000,400 .
